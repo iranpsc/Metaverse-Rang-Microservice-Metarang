d2bebac2e4a1d78bd7b956c0c134cbc3b9996386
mobinnemati <6ix.mobin@gmail.com>
2026-05-31 12:51:10 +0330

    Update settings.py

--- services/article-service/core/core/settings.py
+++ services/article-service/core/core/settings.py
@@ -138,10 +138,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -157,8 +154,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    
+    BASE_DIR / "staticfiles", 
 ]
 
 
